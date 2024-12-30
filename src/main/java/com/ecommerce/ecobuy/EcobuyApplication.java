7dc9036e9f0b877da2d128e669b5d150f3b443a7
Diego Andrés Ramírez Mejía <diego.rm.dev@gmail.com>
2024-12-30 18:55:04 -0500

    feat: create the initial folders

---
+++ src/main/java/com/ecommerce/ecobuy/EcobuyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecobuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecobuy/EcobuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecobuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcobuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
